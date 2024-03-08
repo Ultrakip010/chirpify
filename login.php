c406f810aa55303d5e29307e4fe4b5f2db56f96f
Jaiden <jaidendor010@gmail.com>
2024-03-08 14:19:42 +0100

    creation

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+try {
+    $db = new PDO("mysql:host=localhost;dbname=y", "root", "");
+    if(isset($_POST['inloggen'])) {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+    $password = $_POST['password'];
+    $query = $db->prepare("SELECT * FROM users WHERE username = :username");
+    $query->bindParam(":username", $username);
+    $query->execute();
+    if ($query->rowCount() == 1) {
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+    if (password_verify($password, $result['password'])) {
+    echo "Welkom!";
+    } else {
+      echo "Onjuist wachtwoord!";
+    }
+    } else {
+    echo "Onjuiste gebruikersnaam!";
+    }
+    echo "<br>";
+    }
+} catch (PDOException $e) {
+    die("Error!: " . $e->getMessage());
+}
+header("Location: /PHP/index.html");
---
+++ register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include 'add_user.php'; ?>
+    <form method="POST" action="/PHP/add_user.php">
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="username" required><br>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" required><br>
+
+    <input type="submit" name="verzenden" value="Submit">
+</form>
+</body>
+</html>
