9bcd645a5e9fefebf04a8994b38a6e4f9bebf842
jaiden <jaidendor010@gmail.com>
2024-03-11 22:10:15 +0100

    dhg

--- New-Project.php
+++ New-Project.php
@@ -6,7 +6,7 @@
     $dbname = "y";
 
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=y", $username, $password);
+        $conn = new PDO("mysql:host=$servername;$dbname", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
